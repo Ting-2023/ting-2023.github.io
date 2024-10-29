d563a86aa246e42b4c18ea2975d9a8facd33036f
Ting-2023 <153165725+Ting-2023@users.noreply.github.com>
2024-10-29 20:52:55 +0800

    Deploy static site to GitHub Pages

--- deploy.bat
+++ deploy.bat
@@ -1,16 +1,44 @@
-# 这一行用于关闭命令回显，意味着执行命令时不会在命令行界面显示这些命令。这使得脚本的输出更加整洁。
 @echo off
-# 这一行命令用于切换到名为master的分支。如果你的默认分支是main或其他名称，请相应地更改。
+SETLOCAL
+
+:: 1. 切换到master分支。
 git checkout master
 
-xcopy public\*. .\ /E /Y /I
+:: 2. 识别并删除项目根目录下所有旧的静态文件夹和文件。
+:: 注意：请确保以下命令只删除你希望删除的文件夹和文件。
+rd /s /q categories
+rd /s /q css
+rd /s /q fontawesome
+rd /s /q js
+rd /s /q images
+rd /s /q Page
+rd /s /q posts
+rd /s /q scss
+rd /s /q tags
+:: 根据需要继续添加更多文件夹
 
-rd /s /q public
+del /q *.html
+del /q *.xml
+del /q *.css
+del /q *.js
+del /q *.jpg
+del /q *.png
+:: 根据需要继续添加更多文件类型
 
+:: 3. 将public目录中的新文件移动到根目录。
+xcopy public\* .\ /E /Y /I
+
+:: 4. 添加更改到Git暂存区。
 git add .
 
+:: 5. 提交更改。
 git commit -m "Deploy static site to GitHub Pages"
 
+:: 6. 推送到GitHub。
 git push origin master
 
-git checkout -
+:: 7. 切换回原来的分支。
+:: 注意：你需要替换YOUR_BRANCH_NAME为实际你工作的分支名称
+git checkout master
+
+ENDLOCAL
